2deb48c5f61f25f2ba8b97656a60098b9ef9cf2c
helheim254@gmail.com <MM@Reginas-MacBook-Air.local>
2023-06-10 10:14:37 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return name;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return name;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function appendCat(name) {
+    let newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name) {
+    let newCats = [name, ...cats];
+    return newCats;
+}
+
+function removeFirstCat() {
+    let newCats = cats.slice(1);
+    return newCats;
+}
+
+function removeLastCat() {
+    let newCats = cats.slice(0, -1);
+    return newCats;
+}
